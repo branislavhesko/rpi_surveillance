37fe6cd01c13cb482e151dfeb809e68ae9ddef42
branislavhesko <branislav.hesko@protonmail.ch>
2025-02-18 19:22:19 +0100

    fixes

--- rpi_surveillance/ui/record.py
+++ rpi_surveillance/ui/record.py
@@ -7,8 +7,6 @@ from PIL import Image
 from pydantic import BaseModel
 from io import BytesIO
 
-HOST = "http://localhost:5000"
-
 
 def drawer(record):
     with ui.left_drawer().props('behavior="desktop"') as drawer:
@@ -18,24 +16,16 @@ def drawer(record):
         ui.label("Port")
         ui.input(value=record.port, on_change=lambda e: setattr(record, "port", e.value))
         ui.label("Interval")
-        ui.input(value=record.interval, on_change=lambda e: setattr(record, "interval", e.value))
+        ui.input(value=record.interval, on_change=lambda e: setattr(record, "interval", float(e.value)))
         ui.label("Width")
         ui.select([1024, 1920], value=record.width, on_change=lambda e: setattr(record, "width", int(e.value)))
         ui.label("Height")
         ui.select([768, 1080], value=record.height, on_change=lambda e: setattr(record, "height", int(e.value)))
     return drawer
 
-def on_start_camera():
-    requests.get(f"{HOST}/start")
-    ui.notify("Camera started")
-
-
-def on_stop_camera():
-    requests.get(f"{HOST}/stop")
-    ui.notify("Camera stopped")
 
 class Record(BaseModel):
-    host: str = "localhost"
+    host: str = "10.10.10.13"
     port: int = 5000
     interval: int = 1.0
     width: int = 1024
@@ -55,7 +45,7 @@ def main_page(record):
     image_display = ui.interactive_image(size=(record.width, record.height))
 
     def update_image():
-        response = requests.get(f"{HOST}/capture")
+        response = requests.get(f"{record.url}/capture")
         if response.status_code == 200:
             image_display.set_source(Image.open(BytesIO(response.content)))
         else:
@@ -65,14 +55,14 @@ def main_page(record):
     timer = None
     def start_timer():
         global timer
-        requests.get(f"{HOST}/start")
+        requests.get(f"{record.url}/start")
         timer = ui.timer(record.interval, update_image)
 
 
     def stop_timer():
         global timer
         timer.cancel()
-        requests.get(f"{HOST}/stop")
+        requests.get(f"{record.url}/stop")
 
     start_button.on_click(start_timer)
     stop_button.on_click(stop_timer)
